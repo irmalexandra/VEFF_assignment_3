2b20b37e22408cf572a4227c44ba36291d7b9cd0
R1klus <rikhardur.fridgeirsson@gmail.com>
2020-03-10 10:07:28 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,103 +1,12 @@
-const express = require('express')
+const express = require('express');
 const server = express();
 
-const requester = require("axios")
+const requester = require("axios");
 
 const hostname = 'localhost';
-const port = 3232
+const port = 3232;
 
-const url = "http://" + hostname + ":" + port + "/api/v1/"
-
-server.listen(port, hostname, () => {
-    console.log("listening on port " + port + " and hostname " + hostname)
-});
-
-
-server.get("/api/v1/events", (req, res) => {
-    eventList = makeEventList();
-    res.status(200).send(eventList);
-});
-
-server.get("/api/v1/events/event/:eventID", (req, res) => {
-    
-    var eventID = req.params.eventID
-    var eventObj = findEvent(eventID)
-    
-    res.status(200).send(eventObj);
-});
-
-server.get("/api/v1/events/event/:eventID/bookings", (req, res) => {
-    var eventID = req.params.eventID
-    var eventObj = findEvent(eventID)
-    var bookingsArr = eventObj.bookings
-    var eventBookingsArr = makeBookingList(bookingsArr)
-    res.status(200).send(eventBookingsArr);
-});
-
-server.get("/api/v1/events/event/:eventID/bookings/booking/:bookingID", (req, res) => {
-    eventID = req.params.eventID
-    if (findEvent(eventID) != -1) {
-        var bookingID = req.params.bookingID
-        var eventObj = findEvent(eventID)
-        var bookingsIDArr = eventObj.bookings
-        var booking = findBooking(bookingsIDArr, bookingID)
-        if (booking != -1){
-            res.status(200).send(booking);
-        }
-        else {
-            res.status(404).send("Booking not found for this event.")
-        }
-    }
-    else {
-        res.status(404).send("Event not found.")
-    }
-});
-
-server.use("*", (req, res) => {
-    res.status(405).send("This request is not allowed.")
-});
-
-
-// requests //
-// ----------------------------------------------------------------------- //
-
-requester.get(url + "events", {  
-}) 
-.then((res) => {
-    console.log("get response is working")
-    console.log("Event list: \n" + res.data)
-})
-.catch((error) =>{
-    console.log("event list response error")
-    console.log("error res: " + error)
-})
-
-requester.get(url + "events/event/1") 
-.then((res) => {
-    console.log("Specific event: \n" + res.data)
-})
-.catch((error) =>{
-    console.log("specific event response error")
-    console.log("error res is: " + error)
-})
-
-requester.get(url + "events/event/0/bookings") 
-.then((res) => {
-    console.log("Bookings for event: \n" + res.data)
-})
-.catch((error) =>{
-    console.log("bookings for event response error")
-    console.log("error res is: " + error)
-})
-
-requester.get(url + "events/event/1/bookings/booking/3") 
-.then((res) => {
-    console.log("Specific booking for event: \n" + res.data)
-})
-.catch((error) => {
-    console.log("specific booking response error")
-    console.log("error res is: " + error)
-})
+const url = "http://" + hostname + ":" + port + "/api/v1/";
 
 
 // functions //
@@ -114,42 +23,168 @@ function makeEventList(){
             startDate:events[i].startDate,
             endDate:events[i].endDate
         };
-        eventArr.push(newObj) 
-    };
+        eventArr.push(newObj)
+    }
     return eventArr
-};
+}
 
 function findEvent(id){
     for (var i = 0; i < events.length; i++){
         if (id == events[i].id) {
             return events[i]
-        };
-    };
+        }
+    }
     return -1
-};
+}
 
 function makeBookingList(bookingsArr){
     var eventBookingsArr = []
     for (var i = 0; i < bookings.length; i++){
         if (bookings[i].id = bookingsArr[i]) {
             eventBookingsArr.push(bookings[i])
-        };
-    };
+        }
+    }
     return eventBookingsArr
-};
+}
 
 function findBooking(bookingsIDArr, id){
-    
+
     for (var i = 0; i < bookings.length; i++){
         if (id == bookingsIDArr[i]) {
             return bookings[id]
         }
         else if (i == (bookingsIDArr.length)){
             return -1
-        };
-    };
+        }
+    }
     return -1
-};
+}
+
+function createEvent(eventDetails){
+
+}
+
+function validateCreateInfo(createParams){
+    const isNaN = isNaN(createParams.capacity)
+
+
+}
+
+const createParams = {
+    name: "The Wolf of Wall street",
+    capacity: 30,
+    startDate: new Date(Date.UTC(2020, 03, 10, 22, 0)),
+    endDate: new Date(Date.UTC(2020, 02, 03, 00, 0)),
+    description: "Based on the true story of Jordan Belfort",
+    location: "Bio Paradís, Salur 4"
+}
+
+// Endpoints //
+// ---------------------------------------------------------------------//
+
+// Server listener
+server.listen(port, hostname, () => {
+    console.log("listening on port " + port + " and hostname " + hostname)
+});
+
+// Read All Events
+server.get("/api/v1/events", (req, res) => {
+    eventList = makeEventList();
+    res.status(200).send(eventList);
+});
+
+// Read Individual Event
+server.get("/api/v1/events/event/:eventID", (req, res) => {
+    var eventID = req.params.eventID;
+    var eventObj = findEvent(eventID);
+    
+    res.status(200).send(eventObj);
+});
+
+// Read all Bookings
+server.get("/api/v1/events/event/:eventID/bookings", (req, res) => {
+    var eventID = req.params.eventID;
+    var eventObj = findEvent(eventID);
+    var bookingsArr = eventObj.bookings;
+    var eventBookingsArr = makeBookingList(bookingsArr);
+    res.status(200).send(eventBookingsArr);
+});
+
+// Read Individual Booking
+server.get("/api/v1/events/event/:eventID/bookings/booking/:bookingID", (req, res) => {
+    eventID = req.params.eventID;
+    if (findEvent(eventID) != -1) {
+        var bookingID = req.params.bookingID;
+        var eventObj = findEvent(eventID);
+        var bookingsIDArr = eventObj.bookings;
+        var booking = findBooking(bookingsIDArr, bookingID);
+        if (booking != -1){
+            res.status(200).send(booking);
+        }
+        else {
+            res.status(404).send("Booking not found for this event.")
+        }
+    }
+    else {
+        res.status(404).send("Event not found.")
+    }
+});
+
+// Default for not allowed statements
+server.use("*", (req, res) => {
+    res.status(405).send("This request is not allowed.")
+});
+
+server.get("/api/v1/events/create", (req, res) => {
+    eventList = makeEventList();
+    res.status(200).send(eventList);
+});
+
+
+
+// requests //
+// ----------------------------------------------------------------------- //
+
+requester.get(url + "events", {  
+}) 
+.then((res) => {
+    console.log("get response is working");
+    console.log("Event list: \n" + res.data)
+})
+.catch((error) =>{
+    console.log("event list response error");
+    console.log("error res: " + error)
+})
+
+requester.get(url + "events/event/1") 
+.then((res) => {
+    console.log("Specific event: \n" + res.data)
+})
+.catch((error) =>{
+    console.log("specific event response error");
+    console.log("error res is: " + error)
+});
+
+requester.get(url + "events/event/0/bookings") 
+.then((res) => {
+    console.log("Bookings for event: \n" + res.data)
+})
+.catch((error) =>{
+    console.log("bookings for event response error");
+    console.log("error res is: " + error)
+});
+
+requester.get(url + "events/event/1/bookings/booking/3") 
+.then((res) => {
+    console.log("Specific booking for event: \n" + res.data)
+})
+.catch((error) => {
+    console.log("specific booking response error");
+    console.log("error res is: " + error)
+});
+
+
+
 
 // database //
 // -------------------------------------------------------------------------------------//
