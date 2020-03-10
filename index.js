1cdc3fe35f99577fa2f790d121dcefb0494c6c5f
R1klus <rikhardur.fridgeirsson@gmail.com>
2020-03-10 17:54:11 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 //The following is an example of an array of two events.
 var events = [
-    { id: 430, name: "The Whistlers", description: "Romania, 2019, 97 minutes", location: "Bio Paradís, Salur 1", capacity: 40, startDate: new Date(Date.UTC(2020, 02, 03, 22, 0)), endDate: new Date(Date.UTC(2020, 02, 03, 23, 45)), bookings: [40,23,19] },
+    { id: 430, name: "The Whistlers", description: "Romania, 2019, 97 minutes", location: "Bio Paradís, Salur 1", capacity: 40, startDate: new Date(Date.UTC(2020, 02, 03, 22, 0)), endDate: new Date(Date.UTC(2020, 02, 03, 23, 45)), bookings: [] }, // 40,23,19
     { id: 100, name: "HarpFusion: Bach to the Future", description: "Harp ensemble", location: "Harpa, Hörpuhorn", capacity: 100, startDate: new Date(Date.UTC(2020, 02, 12, 15, 0)), endDate: new Date(Date.UTC(2020, 02, 12, 16, 0)), bookings: [20] }
 ];
 
@@ -50,10 +50,31 @@ function makeEventList(){
     return eventArr
 }
 
-function findEvent(id){
+function findEvent(eventID){
     for (var i = 0; i < events.length; i++){
-        if (id == events[i].id) {
+        if (eventID == events[i].id) {
             return events[i]
+        }
+    }
+    return -1
+}
+
+function findBooking(bookingID){
+    for(var i = 0; i < bookings.length; i++){
+        if(bookingID === bookings[i].id){
+            return bookings[i]
+        }
+    }
+}
+
+function findBookingForEvent(bookingsIDArr, id){
+
+    for (var i = 0; i < bookings.length; i++){
+        if (id === bookingsIDArr[i]) {
+            return bookings[id]
+        }
+        else if (i === (bookingsIDArr.length)){
+            return -1
         }
     }
     return -1
@@ -72,21 +93,8 @@ function makeBookingList(bookingsArr){
     return eventBookingsArr
 }
 
-function findBooking(bookingsIDArr, id){
-
-    for (var i = 0; i < bookings.length; i++){
-        if (id === bookingsIDArr[i]) {
-            return bookings[id]
-        }
-        else if (i === (bookingsIDArr.length)){
-            return -1
-        }
-    }
-    return -1
-}
-
 function createEvent(eventDetails){
-    if (validateCreateEventInfo(eventDetails)){
+    if (validateEventInfo(eventDetails)){
         var dateArr = eventDetails.startDate.split(" ");
         var sdate = dateArr[0]
         var stime = dateArr[1]
@@ -132,57 +140,8 @@ function createEvent(eventDetails){
     }
 }
 
-function validateCreateEventInfo(eventDetails){
-    let capOverOrEqualZero = false;
-    const validStartDate = (new Date(eventDetails.startDate)).getTime() > 0;
-
-    const validEndDate = (new Date(eventDetails.endDate)).getTime() > 0;
-
-    const validCap = Number.isInteger(parseInt(eventDetails.capacity));
-    if(validCap){capOverOrEqualZero = parseInt(eventDetails.capacity) >= 0;}
-
-    if(validStartDate && validEndDate && validCap && capOverOrEqualZero){return true}
-    return false
-}
-
-function generateEventID(){
-    var eventID = eventsLength
-    eventsLength++
-    return eventID
-}
-
-function generateBookingID(){
-    var bookingID = bookingsLength
-    bookingsLength++
-    return bookingID
-}
-
-function checkSpots(spots, eventID){
-    let currentEvent = findEvent(eventID);
-    let currentBookingsIDs = currentEvent.bookings;
-    let currentBookings = makeBookingList(currentBookingsIDs);
-    let availableSpots = currentEvent.capacity
-    for(var i = 0; i < currentBookings.length; i++){
-        availableSpots -= currentBookings[i].spots
-    }
-    availableSpots -= spots
-    if(availableSpots > 0){return true}
-    return false
-}
-
-function validateCreateBookingInfo(bookingDetails){
-    if(bookingDetails.tel != undefined || bookingDetails.email != undefined){
-        let spotsOverZero = bookingDetails.spots > 0;
-        let validSpots = false;
-        if(spotsOverZero){validSpots = checkSpots(bookingDetails.spots, bookingDetails.eventID)}
-        if(spotsOverZero && validSpots){return true}
-    }
-    return false
-
-}
-
 function createBooking(bookingDetails){
-    if(validateCreateBookingInfo(bookingDetails)){
+    if(validateBookingInfo(bookingDetails)){
         if(bookingDetails.tel == undefined){bookingDetails.tel = ""}
         if(bookingDetails.email == undefined){bookingDetails.email = ""}
         let booking = {
@@ -203,6 +162,62 @@ function createBooking(bookingDetails){
     }
 }
 
+function validateEventInfo(eventDetails){
+    let capOverOrEqualZero = false;
+    const validStartDate = (new Date(eventDetails.startDate)).getTime() > 0;
+
+    const validEndDate = (new Date(eventDetails.endDate)).getTime() > 0;
+
+    const validCap = Number.isInteger(parseInt(eventDetails.capacity));
+    if(validCap){capOverOrEqualZero = parseInt(eventDetails.capacity) >= 0;}
+
+    if(validStartDate && validEndDate && validCap && capOverOrEqualZero){return true}
+    return false
+}
+
+function validateBookingInfo(bookingDetails){
+    if(bookingDetails.tel != undefined || bookingDetails.email != undefined){
+        let spotsOverZero = bookingDetails.spots > 0;
+        let validSpots = false;
+        if(spotsOverZero){validSpots = checkSpots(bookingDetails.spots, bookingDetails.eventID)}
+        if(spotsOverZero && validSpots){return true}
+    }
+    return false
+
+}
+
+function checkSpots(spots, eventID){
+    let currentEvent = findEvent(eventID);
+    let currentBookingsIDs = currentEvent.bookings;
+    let currentBookings = makeBookingList(currentBookingsIDs);
+    let availableSpots = currentEvent.capacity
+    for(var i = 0; i < currentBookings.length; i++){
+        availableSpots -= currentBookings[i].spots
+    }
+    availableSpots -= spots
+    if(availableSpots > 0){return true}
+    return false
+}
+
+function generateEventID(){
+    var eventID = eventsLength
+    eventsLength++
+    return eventID
+}
+
+function generateBookingID(){
+    var bookingID = bookingsLength
+    bookingsLength++
+    return bookingID
+}
+
+function findArrayIndex(eventID, arr){
+    for(var i = 0; i < arr.length; i++){
+        if(eventID === arr[i].id){return i}
+    }
+    return -1
+}
+
 function deleteAllEvents(){
     if(events.length > 0){
         let retEvents = [];
@@ -210,7 +225,7 @@ function deleteAllEvents(){
             let bookingsArr = makeBookingList(events[i].bookings)
             events[i].bookings = bookingsArr;
         }
-        retEvents = events.slice();
+        retEvents = events;
         events = []
         return retEvents;
     }
@@ -221,23 +236,58 @@ function deleteEvent(eventIDstr){
     let eventID = parseInt(eventIDstr);
     let retEvent = findEvent(eventID);
     if(retEvent !== -1 && retEvent.bookings.length === 0) {
-        let eventIndex = findEventIndex(eventID);
+        let eventIndex = findArrayIndex(eventID, events);
         if (eventIndex !== -1) {
             events.splice(eventIndex, 1);
             return retEvent
-        } else {
-            return -1
         }
     }
     return -1
 }
 
-function findEventIndex(eventID){
-    for(var i = 0; i < events.length; i++){
-        if(eventID === events[i].id){return i}
+function deleteBooking(bookingIDstr){
+    let bookingID = parseInt(bookingIDstr)
+    let retBooking = findBooking(bookingID)
+    if(retBooking !== -1){
+        let bookingIndex = findArrayIndex(bookingID, bookings)
+        if(bookingIndex !== -1){
+            bookings.splice(bookingIndex, 1)
+            return retBooking
+        }
     }
     return -1
 }
+
+function deleteAllBookings(eventID){
+    if(bookings.length > 0){
+        let currentEvent = findEvent(eventID);
+        let retBookings = currentEvent.bookings;
+        currentEvent.bookings = [];
+        return retBookings;
+    }
+    return -1
+}
+
+function updateEvent(updatedEvent){
+    let eventID = updatedEvent.eventID
+    if(eventID !== undefined){
+        let currentEvent = findEvent(eventID)
+        if(currentEvent.bookings.length === 0){
+            if(validateEventInfo(updatedEvent)){
+                currentEvent.name = updatedEvent.name
+                currentEvent.capacity = updatedEvent.capacity
+                currentEvent.startDate = updatedEvent.startDate
+                currentEvent.endDate = updatedEvent.endDate
+                currentEvent.location = updatedEvent.location
+                currentEvent.description = updatedEvent.description
+                return currentEvent
+            }
+        }
+
+    }
+    return -1
+}
+
 
 // Endpoints //
 // ---------------------------------------------------------------------//
@@ -277,7 +327,7 @@ server.get("/api/v1/events/event/:eventID/bookings/booking/:bookingID", (req, re
         var bookingID = req.params.bookingID;
         var eventObj = findEvent(eventID);
         var bookingsIDArr = eventObj.bookings;
-        var booking = findBooking(bookingsIDArr, bookingID);
+        var booking = findBookingForEvent(bookingsIDArr, bookingID);
         if (booking !== -1){
             res.status(200).send(booking);
         }
@@ -327,15 +377,63 @@ server.delete("/api/v1/events/event/:eventID/deleteEvent", (req , res) => {
     }
 });
 
-// Delete All
+// Delete Booking
+server.delete("/api/v1/events/event/:eventID/bookings/booking/:bookingID/deleteBooking", (req, res) => {
+    let bookingID = req.params.bookingID
+    let retBooking = deleteBooking(bookingID)
+    if(retBooking !== -1){
+        res.status(200).send(retBooking)
+    }
+    else{
+        res.status(400).send("Invalid booking info")
+    }
+});
+
+// Delete All Events
 server.delete("/api/v1/events/deleteAllEvents", (req, res) =>{
     let retEvents = deleteAllEvents()
     if(retEvents !== -1){
         res.status(200).send(retEvents)
     }
     else{
-        res.status(400).send(retEvents)
+        res.status(400).send("Error when deleting all events")
     }
+});
+
+// Delete all bookings for event
+server.delete("/api/v1/events/event/:eventID/bookings/deleteAllBookings", (req, res) => {
+    let eventID = req.params.eventID
+    let retBookings = deleteAllBookings(eventID)
+    if(retBookings !== -1){
+        res.status(200).send(retBookings)
+    }
+    else{
+        res.status(400).send("Error when deleting all bookings for event "+ eventID)
+    }
+});
+
+// Delete Booking
+server.delete("/api/v1/events/event/:eventID/bookings/booking/:bookingID/deleteBooking", (req, res) => {
+    let bookingID = req.params.bookingID
+    let retBooking = deleteBooking(bookingID)
+    if(retBooking !== -1){
+        res.status(200).send(retBooking)
+    }
+    else{
+        res.status(400).send("Invalid booking info")
+    }
+});
+
+// Update
+server.put("/api/v1/events/event/updateEvent", (req, res) => {
+    let updatedEvent = updateEvent(req.body)
+    if(updatedEvent !== -1){
+        res.status(200).send(updatedEvent)
+    }
+    else{
+        res.status(400).send(updatedEvent)
+    }
+
 });
 
 // Default for not allowed statements
@@ -350,15 +448,49 @@ server.use("*", (req, res) => {
 // ----------------------------------------------------------------------- //
 //
 
-requester.delete(url + "events/deleteAllEVents")
-    .then((res) => {
-        console.log("Delete all events successful")
-        console.log("Events deleted :"+ res.data)
-    })
-    .catch((error) => {
-        console.log("Delete events not successfull")
-        console.log("error is "+ error)
-    })
+// requester.put(url+"events/event/updateEvent", {
+//     eventID : 430,
+//     name : "newNAme",
+//     capacity : 200,
+//     location : "Emils house?",
+//     description: "newdesc",
+//     startDate: new Date(Date.UTC(20150, 02, 03, 22, 0)),
+//     endDate: new Date(Date.UTC(2120, 02, 03, 23, 45))
+// })
+//     .then((res) => {
+//         console.log("updated event : " + res.data)
+//     })
+//     .catch((error) => {
+//         console.log(("Error while updating event: "+error))
+//     });
+
+// requester.delete(url + "events/deleteAllEVents")
+//     .then((res) => {
+//         console.log("Delete all events successful")
+//         console.log("Events deleted :"+ res.data)
+//     })
+//     .catch((error) => {
+//         console.log("Delete all events not successfull")
+//         console.log("error is "+ error)
+//     })
+
+// requester.delete(url+"events/event/100/bookings/booking/23/deleteBooking")
+//     .then((res) => {
+//         console.log("Deleted booking: "+res.data)
+//     })
+//     .catch((error) => {
+//         console.log("delete booking response error")
+//         console.log("error is: "+ error)
+//     });
+
+// requester.delete(url+"events/event/430/bookings/deleteAllBookings")
+//     .then((res) => {
+//         console.log("Deleted all bookings for event")
+//     })
+//     .catch((error) => {
+//         console.log("Delete all bookings error")
+//         console.log("Error is:"+error)
+//     })
 
 // requester.post(url + "events/event/bookings/createBooking",{
 //     eventID: 0,
@@ -376,13 +508,13 @@ requester.delete(url + "events/deleteAllEVents")
 //     });
 
 // requester.delete(url+"events/event/100/deleteEVent")
-//     .then((res) => {
-//         console.log("Deleted event: "+res.data)
-//     })
-//     .catch((error) => {
-//         console.log("delete event response error")
-//         console.log("error is: "+ error)
-//     });
+// //     .then((res) => {
+// //         console.log("Deleted event: "+res.data)
+// //     })
+// //     .catch((error) => {
+// //         console.log("delete event response error")
+// //         console.log("error is: "+ error)
+// //     });
 
 // requester.post(url + "events/createEvent",{
 //     name: "The Wolf of Wall Street",
